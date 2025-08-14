8fbf812373dcfbd127ab6c546f0e18be5aa98fa7
FM Boire <46877544+fmboire@users.noreply.github.com>
2025-08-14 08:23:01 -0400

    Update site

--- generate_ics.py
+++ generate_ics.py
@@ -1,7 +1,49 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from zoneinfo import ZoneInfo
 import os
 
+TZ = ZoneInfo("America/Toronto")
+
+def create_ics(filename, uid, start_dt, duration_hours, summary, location, url):
+    # attach local tz to the naive datetime
+    start_local = start_dt.replace(tzinfo=TZ)
+    end_local = start_local + timedelta(hours=duration_hours)
+
+    # convert to UTC for the ICS
+    start_utc = start_local.astimezone(timezone.utc)
+    end_utc = end_local.astimezone(timezone.utc)
+
+    dtstamp = datetime.now(timezone.utc).strftime('%Y%m%dT%H%M%SZ')
+    dtstart = start_utc.strftime('%Y%m%dT%H%M%SZ')
+    dtend   = end_utc.strftime('%Y%m%dT%H%M%SZ')
+
+    ics_content = f"""BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//University of Ottawa//Colloquium Series//EN
+BEGIN:VEVENT
+UID:{uid}
+DTSTAMP:{dtstamp}
+DTSTART:{dtstart}
+DTEND:{dtend}
+SUMMARY:{summary}
+LOCATION:{location}
+URL:{url}
+END:VEVENT
+END:VCALENDAR
+"""
+    with open(filename, 'w', encoding='utf-8', newline="") as f:
+        f.write(ics_content)
+
 talks = [
+    {
+        "filename": "pumpluen_fall2025.ics",
+        "uid": "20250904-bartolucci@uottawa.ca",
+        "date": datetime(2025, 9, 4, 13, 0),
+        "duration_hours": 1,
+        "summary": "Talk by Susanne Pumpluen",
+        "location": "University of Ottawa, STEM building, room 664",
+        "url": "https://www.nottingham.ac.uk/mathematics/people/susanne.pumpluen"
+    },
     {
         "filename": "bartolucci_fall2025.ics",
         "uid": "20250911-bartolucci@uottawa.ca",
@@ -40,28 +82,6 @@ talks = [
     }
 ]
 
-def create_ics(filename, uid, start_dt, duration_hours, summary, location, url):
-    dtstamp = datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
-    dtstart = start_dt.strftime('%Y%m%dT%H%M%SZ')
-    dtend = (start_dt + timedelta(hours=duration_hours)).strftime('%Y%m%dT%H%M%SZ')
-
-    ics_content = f"""BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//University of Ottawa//Colloquium Series//EN
-BEGIN:VEVENT
-UID:{uid}
-DTSTAMP:{dtstamp}
-DTSTART:{dtstart}
-DTEND:{dtend}
-SUMMARY:{summary}
-LOCATION:{location}
-URL:{url}
-END:VEVENT
-END:VCALENDAR
-"""
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(ics_content)
-    print(f"Created {filename}")
 
 os.makedirs("calendar", exist_ok=True)
 
